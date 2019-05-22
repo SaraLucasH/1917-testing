825612700a50feadcc00fd235c992c4ff3f1788b
SaraLucasH <47327009+SaraLucasH@users.noreply.github.com>
2019-05-22 11:29:13 +0200

    Test 3 y test 2 (pequeña modificacion)
    Se ha solucionado el fallo del test 3, pues no descargaba la dependencia de bonigarcia correctamente. Se ha corregido algun detalle del test 2 para no repetir codigo y el uso de una misma libreria (assertJ) en los 3 tipos de test.

--- tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/PruebaTres_Sistema.java
+++ tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/PruebaTres_Sistema.java
@@ -1,9 +1,12 @@
 package es.codeurjc.ais.tictactoe;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,11 +14,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-class PruebaTres_Sistema {
+public class PruebaTres_Sistema {
 	//Dos pestañas dos driver
 	protected WebDriver driver1,driver2;
 	//Dado que es necesario cierto retardo para que las acciones no sean inmediatas hemos usado WebDriverWait
@@ -41,9 +45,9 @@ class PruebaTres_Sistema {
 		System.out.println(driver1.findElement(By.id("nickname")));
 		driver1.findElement(By.id("nickname")).sendKeys("Sara");
 		driver2.findElement(By.id("nickname")).sendKeys("Ioana");
-		waitDriver1=new WebDriverWait(driver1,10);
-		waitDriver2=new WebDriverWait(driver2,10);
-		botonEntrada1=driver1.findElement(By.className("btn btn-lg"));
+		waitDriver1=new WebDriverWait(driver1,7);
+		waitDriver2=new WebDriverWait(driver2,7);
+		botonEntrada1=driver1.findElement(By.id("startBtn"));
 		botonEntrada2=driver2.findElement(By.id("startBtn"));
 		botonEntrada1.click();
 		botonEntrada2.click();
@@ -51,7 +55,7 @@ class PruebaTres_Sistema {
 	}
 	
 	@After
-	public void finish() {
+	public void teardown() {
 		if(driver1!=null) {
 			driver1.quit();
 		}
@@ -60,15 +64,14 @@ class PruebaTres_Sistema {
 		}
 	}
 	
-	/*@AfterClass
+	@AfterClass
 	public static void finishDriver() {
 		WebApp.stop();
 	}
 	
-	@Test
+	/*@Test
 	public void haGanadoXTest() {	
 		
-		System.out.println(botonEntrada1);
 		boolean selectBotonEntrada2=botonEntrada1.isSelected();
 		boolean selectBotonEntrada1=botonEntrada2.isSelected();
 		waitDriver1.until(ExpectedConditions.elementSelectionStateToBe(botonEntrada2, selectBotonEntrada1));
@@ -84,20 +87,58 @@ class PruebaTres_Sistema {
 		waitDriver1.until(ExpectedConditions.alertIsPresent());
 		waitDriver2.until(ExpectedConditions.alertIsPresent());
 		//Mensaje del ganador y perdedor igual en ambas pantallas
-		assertThat(driver1.switchTo().alert().getText()).isEqualTo(driver2.switchTo().alert().getText());		
-		assertThat("sara wins").isSubstringOf(driver1.switchTo().alert().getText());
-		assertThat("ioana looses").isSubstringOf(driver1.switchTo().alert().getText());
-	}
-	
-	@Test
-	void haPerdidoXTest() {
-		
-	}
-	
-	@Test
-	void empateTest() {
-		
+		assertThat(driver1.switchTo().alert().getText()).isEqualTo(driver2.switchTo().alert().getText());			
+		assertThat("Sara wins!").isSubstringOf(driver1.switchTo().alert().getText());
+		assertThat("Ioana looses").isSubstringOf(driver1.switchTo().alert().getText());
 	}*/
+	
+	/*@Test
+	public void haPerdidoXTest() {		
+		boolean selectBotonEntrada2=botonEntrada1.isSelected();
+		boolean selectBotonEntrada1=botonEntrada2.isSelected();
+		waitDriver1.until(ExpectedConditions.elementSelectionStateToBe(botonEntrada2, selectBotonEntrada1));
+		waitDriver2.until(ExpectedConditions.elementSelectionStateToBe(botonEntrada1, selectBotonEntrada2));
+		//Cuando ya los dos jugadores estan iniciados
+		
+		driver1.findElement(By.id("cell-0")).click();	
+		driver2.findElement(By.id("cell-2")).click();
+		driver1.findElement(By.id("cell-1")).click();	
+		driver2.findElement(By.id("cell-4")).click();
+		driver1.findElement(By.id("cell-3")).click();
+		driver2.findElement(By.id("cell-6")).click();
+		
+		waitDriver1.until(ExpectedConditions.alertIsPresent());
+		waitDriver2.until(ExpectedConditions.alertIsPresent());
+		//Mensaje del ganador y perdedor igual en ambas pantallas		
+		assertThat(driver1.switchTo().alert().getText()).isEqualTo(driver2.switchTo().alert().getText());		
+		assertThat("Ioana wins!").isSubstringOf(driver1.switchTo().alert().getText());
+		assertThat("Sara looses").isSubstringOf(driver1.switchTo().alert().getText());
+	}*/
+	
+	@Test
+	public void empateTest() {
+		boolean selectBotonEntrada2=botonEntrada1.isSelected();
+		boolean selectBotonEntrada1=botonEntrada2.isSelected();
+		waitDriver1.until(ExpectedConditions.elementSelectionStateToBe(botonEntrada2, selectBotonEntrada1));
+		waitDriver2.until(ExpectedConditions.elementSelectionStateToBe(botonEntrada1, selectBotonEntrada2));
+		//Cuando ya los dos jugadores estan iniciados
+		
+		driver1.findElement(By.id("cell-0")).click();	
+		driver2.findElement(By.id("cell-2")).click();
+		driver1.findElement(By.id("cell-1")).click();	
+		driver2.findElement(By.id("cell-3")).click();
+		driver1.findElement(By.id("cell-4")).click();
+		driver2.findElement(By.id("cell-7")).click();
+		driver1.findElement(By.id("cell-5")).click();
+		driver2.findElement(By.id("cell-8")).click();
+		driver1.findElement(By.id("cell-6")).click();		
+		
+		waitDriver1.until(ExpectedConditions.alertIsPresent());
+		waitDriver2.until(ExpectedConditions.alertIsPresent());
+		//Mensaje empate igual en ambas pantallas		
+		assertThat(driver1.switchTo().alert().getText()).isEqualTo(driver2.switchTo().alert().getText());		
+		assertThat("Draw!").contains(driver1.switchTo().alert().getText());		
+	}
 
 	
 }
--- tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/TicTacToeDoblesTest.java
+++ tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/TicTacToeDoblesTest.java
@@ -1,23 +1,17 @@
 package es.codeurjc.ais.tictactoe;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.hasItems;
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-
 import es.codeurjc.ais.tictactoe.Connection;
 import es.codeurjc.ais.tictactoe.Player;
 import es.codeurjc.ais.tictactoe.TicTacToeGame;
 import es.codeurjc.ais.tictactoe.TicTacToeGame.EventType;
-import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerResult;
 import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerValue;
-
 import static org.mockito.Mockito.*;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
-
-import org.hamcrest.*;
 
 public class TicTacToeDoblesTest {
 	private TicTacToeGame game;
@@ -25,6 +19,12 @@ public class TicTacToeDoblesTest {
 	private Connection connection2;
 	private Player player1;
 	private Player player2;
+	
+	private void verificacionTurnoInicial() {
+		//verificamos que los turnos iniciales son los correctos 
+		verify(connection1,times(1)).sendEvent(EventType.SET_TURN, game.getPlayers().get(0));
+		verify(connection2,times(0)).sendEvent(EventType.SET_TURN, game.getPlayers().get(1));
+	}
 	
 	@Before
 	public void setUp() {
@@ -43,71 +43,56 @@ public class TicTacToeDoblesTest {
 		verify(connection2,times(2)).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(player1,player2)));
 	}
 	
-	
-	/*
-	 * X |   | O
-	 *   | X |
-	 *   | O | X
-	 */
 	@Test
 	public void haGanadoXTest() {
-		
-		//verificamos que los turnos iniciales son los correctos 
-		verify(connection1,times(1)).sendEvent(EventType.SET_TURN, game.getPlayers().get(0));
-		verify(connection2,times(0)).sendEvent(EventType.SET_TURN, game.getPlayers().get(1));
-		
-		//se va marcando la casilla correspondiete y se verifican los turnos 
+		verificacionTurnoInicial();
+		//Se va marcando la casilla correspondiete y se verifican los turnos 
 		game.mark(0);
 		verify(connection1, times(1)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(1)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(2);
+		game.mark(3);
 		verify(connection1, times(2)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(1)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(4);
+		game.mark(1);
 		verify(connection1, times(2)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(2)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(7);
+		game.mark(4);
 		verify(connection1, times(3)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(2)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(8);
+		game.mark(2);
 		verify(connection1, times(3)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(2)).sendEvent(EventType.SET_TURN, player2);
 		
 		ArgumentCaptor<WinnerValue> argument = ArgumentCaptor.forClass(WinnerValue.class);
 		
 		//comprobamos que el array con las posiciones del tablero del jugador victoriosos son las mismas
-		int[] tableroJugador1 = {0,4,8};
-		assertArrayEquals(tableroJugador1, game.checkWinner().pos);
 		
+		assertThat(new int[] {0,1,2}).isEqualTo( game.checkWinner().pos);		
 		verify(connection1).sendEvent(eq(EventType.GAME_OVER), argument.capture());
 		verify(connection2, times(1)).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-	}
+	}	
+	//Simulacion:
+	// X X X
+	// O O 
+	//
 	
-	/*
-	 * O |   | 
-	 * O | X | X
-	 * O |   | X
-	 */ 
 	@Test
 	public void haPerdidoXTest() {
-		//verificamos que los turnos iniciales son los correctos 
-		verify(connection1,times(1)).sendEvent(EventType.SET_TURN, game.getPlayers().get(0));
-		verify(connection2,times(0)).sendEvent(EventType.SET_TURN, game.getPlayers().get(1));
+		verificacionTurnoInicial();
 		
-		//se va marcando la casilla correspondiete y se verifican los turnos 
-		game.mark(4);
+		game.mark(0);
 		verify(connection1, times(1)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(1)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(0);
+		game.mark(2);
 		verify(connection1, times(2)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(1)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(5);
+		game.mark(1);
 		verify(connection1, times(2)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(2)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(3);
+		game.mark(4);
 		verify(connection1, times(3)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(2)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(8);
+		game.mark(3);
 		verify(connection1, times(3)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(3)).sendEvent(EventType.SET_TURN, player2);
 		game.mark(6);
@@ -117,25 +102,19 @@ public class TicTacToeDoblesTest {
 		ArgumentCaptor<WinnerValue> argument = ArgumentCaptor.forClass(WinnerValue.class);
 		
 		//comprobamos que el array con las posiciones del tablero del jugador victoriosos son las mismas
-		int[] tableroJugador2 = {0,3,6};
-		assertArrayEquals(tableroJugador2, game.checkWinner().pos);
 		
+		assertThat(new int[] {6,4,2}).isEqualTo( game.checkWinner().pos);	
 		verify(connection1,times(1)).sendEvent(eq(EventType.GAME_OVER), argument.capture());
 		verify(connection2).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-	}
+	}	
+	//X X O
+	//X O 
+	//O 
 	
-	
-	/* X | X | O
-	 * O | X | X
-	 * X | O | O
-	 */
 	@Test
 	public void empateTest() {
-		//verificamos que los turnos iniciales son los correctos 
-		verify(connection1,times(1)).sendEvent(EventType.SET_TURN, game.getPlayers().get(0));
-		verify(connection2,times(0)).sendEvent(EventType.SET_TURN, game.getPlayers().get(1));
+		verificacionTurnoInicial();
 		
-		//se va marcando la casilla correspondiete y se verifican los turnos 
 		game.mark(0);
 		verify(connection1, times(1)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(1)).sendEvent(EventType.SET_TURN, player2);
@@ -154,25 +133,26 @@ public class TicTacToeDoblesTest {
 		game.mark(7);
 		verify(connection1, times(4)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(3)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(6);
+		game.mark(5);
 		verify(connection1, times(4)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(4)).sendEvent(EventType.SET_TURN, player2);
 		game.mark(8);
 		verify(connection1, times(5)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(4)).sendEvent(EventType.SET_TURN, player2);
-		game.mark(5);
+		game.mark(6);
 		verify(connection1, times(5)).sendEvent(EventType.SET_TURN, player1);
 		verify(connection2, times(4)).sendEvent(EventType.SET_TURN, player2);
 		
-		//comprobamos que se ha producido el empate
-		assertTrue(game.checkDraw());
-		assertFalse(game.checkWinner().win);
+		//Comprobamos que se ha producido el empate
+		assertThat(game.checkDraw()).isTrue();
+		assertThat(game.checkWinner().pos).isNull();
 		
 		ArgumentCaptor<WinnerValue> argument = ArgumentCaptor.forClass(WinnerValue.class);
 		verify(connection1).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-		verify(connection2,times(1)).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-				
+		verify(connection2,times(1)).sendEvent(eq(EventType.GAME_OVER), argument.capture());				
 	}
-	
+	//X X O
+	//O X X
+	//X O O
 	
 }
